1f01d093c9ee2d1b84df16fb57287a33cea32401
TajwarSaiyeed <rjabid36@gmail.com>
2023-01-03 11:05:27 +0600

    update

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const app = express();
 
 const route = require("./route");
+const { addUser } = require("./users");
 
 app.use(
   cors({
@@ -21,6 +22,34 @@ const io = new Server(server, {
     origin: "*",
     methods: ["GET", "POST"],
   },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("join", ({ name, room }) => {
+    socket.join(room);
+
+    const { user } = addUser({ name, room });
+
+    socket.emit("message", {
+      data: {
+        user: { name: "Admin" },
+        message: `Welcome, ${user.name}`,
+      },
+    });
+
+    socket.broadcast.to(user.room).emit("message", {
+      data: {
+        user: { name: "Admin" },
+        message: `${user.name} has joined the chat`,
+      },
+    });
+  });
+
+  io.on("disconnect", () => {
+    console.log("User disconnected");
+  });
 });
 
 server.listen(PORT, () => {
